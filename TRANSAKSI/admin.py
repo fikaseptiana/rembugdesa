a4d1f316a46f0710f8469fc4ea8978e795479b84
hardikwidi <komporpika@gmail.com>
2015-08-28 19:29:55 +0700

    test lagi

--- TRANSAKSI/admin.py
+++ TRANSAKSI/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import Kabupaten, Kecamatan, Kelurahan, KecamatanResource, KelurahanResource, KabupatenResource, Provinsi, ProvinsiResource, \
+    DusunResource, Dusun
+from TRANSAKSI.models import Jadwal
 
-# Register your models here.
+class JadwalAdmin(ImportExportModelAdmin):
+    list_display = ['jenis_kegiatan', 'tanggal', 'tempat', 'pic', 'contact', 'anggota', 'repeatable', 'dusun']
+    resource_class = JadwalResource
+    pass
+
+admin.site.register(Jadwal, JadwalAdmin)
--- TRANSAKSI/views.py
+++ TRANSAKSI/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from MASTER.models import Kabupaten, Kecamatan ,Kelurahan, Provinsi, Dusun
+from MASTER.serializers import KecamatanSerializer, KelurahanSerializer, KabupatenSerializer, ProvinsiSerializer, \
+    DusunSerializer
+from TRANSAKSI.models import Jadwal
+from TRANSAKSI.serializers import JadwalSerializer
+
+
+class JadwalViewSet(viewsets.ModelViewSet):
+    queryset = Jadwal.objects.all()
+    serializer_class= JadwalSerializer
